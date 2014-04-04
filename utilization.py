46f0ddead148961a1343f44df23d7a3478238e46
mileshq <mileshq@gmail.com>
2014-04-04 10:01:26 -0400

    Working on argparse

--- utilization.py
+++ utilization.py
@@ -17,11 +17,6 @@ HOLIDAY_DAYS = ['1/1', '1/20', '2/17', '5/26', '7/4', '9/1', '11/27', '11/28', '
 HOLIDAY_DAYS_COUNT = len(HOLIDAY_DAYS)
 PTO_DAYS_COUNT = 20
 
-# create Timesheets class
-# is iterable, implements next
-# def __iter__(self): returns Day
-# def next(self): returns mon, tue, wed, thu ...
-
 class TimesheetEntry(object):
 
 	def __init__(self, date, project, state, hours):
@@ -46,6 +41,9 @@ class Timesheets(object):
 			for line in fh:
 				self.timesheets.append(TimesheetEntry(**json.loads(line)))
 
+	def add_timesheet_entry(self, timesheet_entry):
+		self.timesheets.append(timesheet_entry)
+
 	def sum_project_hours(self, project='*'):
 		return sum([t.hours for t in self.timesheets
 			if project in [t.project, '*']])
@@ -54,23 +52,50 @@ class Timesheets(object):
 		return sum(t.hours for t in self.timesheets
 			if t.project not in [LABEL_PTO, LABEL_HOLIDAY, LABEL_TRAINING, LABEL_UNUTILIZED])
 
+	# this could be more efficient
+	def percent_utilized_hours(self):
+		return float(self.sum_utilized_hours()) / float(self.sum_project_hours()) * 100
+
+	# def __iter__(self): returns TimesheetEntry
+	# is iterable, implements next()
+	# might be pythonic way to do interation below in persist
+	def persist(self, db_file=TIMESHEETS_FILE):
+		with open(db_file, 'w') as fh:
+			for t in self.timesheets:
+					fh.write(str(t) + "\n")
+
 def parse_args():
 	parser = argparse.ArgumentParser(description="Capture and confirm utilization.")	
-	parser.add_argument('date')
-	parser.add_argument('project')
-	parser.add_argument('state')
-	parser.add_argument('hours', type=int, default=8)
+	#exclusive_group = parser.mutually_exclusive_group()
+	add_entry_group = parser.add_argument_group()
+	add_entry_group.add_argument('-a', '--add', help='Add new timesheet entry.')
+	add_entry_group.add_argument('date')
+	add_entry_group.add_argument('project')
+	add_entry_group.add_argument('state')
+	add_entry_group.add_argument('hours', type=int, default=8)
 	#parser.add_argument(dest='day_count')
 
 	args = parser.parse_args()
-	print TimesheetEntry(**args.__dict__)
+	
+	if args.add:
+		return TimesheetEntry(**args.__dict__)
+		
 
 def main():
 	timesheets = Timesheets()
+	print "Loaded {} timesheets.".format(len(timesheets.timesheets))
+	new_timesheet = parse_args()
+	print "Adding new timesheet: {}".format(new_timesheet.__dict__)
+	add_timesheet_entry(new_timesheet)
+	print "Total timesheets: {}".format(len(timesheets.timesheets))
+
+	#timesheets.timesheets.sort()
+	#print timesheets.timesheets
 	#parse_args()
 
 	print "Total: {}".format(timesheets.sum_project_hours())
 	print "Utilized: {}".format(timesheets.sum_utilized_hours())
+	print "Utilized: {}%".format(timesheets.percent_utilized_hours())
 
 if __name__ == '__main__':
 	main()
